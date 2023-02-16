62549ab703db749b6fc1d107c22b05c3c795eb97
Oleksii Verbytskyi <alverba88@gmail.com>
2023-02-16 18:06:53 +0200

    mod auth

---
+++ src/components/Form/AuthForm/AuthForm.jsx
@@ -0,0 +1,16 @@
+import styles from './AuthForm.module.scss';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export const AuthForm = () => {
+
+  return (
+   <Form>
+     <Form.Group>
+       <Form.Label></Form.Label>
+       <Form.Control/>
+       <Form.Text></Form.Text>
+     </Form.Group>
+   </Form>
+  )
+}
